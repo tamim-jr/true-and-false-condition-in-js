5327f9e8a3f2bb371700325628ef204c41ea8be9
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-10 22:14:28 +0600

    Condition in JavaScript

---
+++ index.js
@@ -0,0 +1,32 @@
+let numberOne = 2;
+let numberTwo = 2;
+if (numberOne === numberTwo){
+    console.log("This result is true.")
+}
+else{
+    console.log("This result is false.")
+}
+let numberOne1 = 2;
+let numberTwo2 = "2";
+if (numberOne1 === numberTwo2){
+    console.log("This result is true.")
+}
+else{
+    console.log("This result is false.")
+}
+let numberOne3 = 0;
+let numberTwo4 = false;
+if (numberOne3 == numberTwo4){
+    console.log("This result is true.")
+}
+else{
+    console.log("This result is false.")
+}
+let numberOne5 = 1;
+let numberTwo6 = true;
+if (numberOne5 === numberTwo6){
+    console.log("This result is true.")
+}
+else{
+    console.log("This result is false.")
+}
